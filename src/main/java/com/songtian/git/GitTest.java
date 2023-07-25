fe7c7c7c3ed3c5a7d3cc78019a33003d87bd3bd4
awei <2392331716@qq.com>
2023-07-25 22:10:54 +0800

    new1

--- src/main/java/com/songtian/git/GitTest.java
+++ src/main/java/com/songtian/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("new day");
         System.out.println("new day");
         System.out.println("new day");
+        System.out.println("new1");
     }
 }
